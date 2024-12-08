8e5a1fbe57b063e657b9bbc8e5cb2fb65a34ef3d
kipash-prog <kidus.UGR-0110-15@aau.edu.et>
2024-12-08 22:28:21 +0300

    Update settings.py

--- user_Auth/settings.py
+++ user_Auth/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+tc*3vf*+qk_o)k^03r(8=yqu0=k@h1nx$zdd(*c6&qvet*8+e'
+SECRET_KEY = 'add your security key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
